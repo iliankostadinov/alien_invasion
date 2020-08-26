9978e2d5eb28fd04dd121fa248fc6a40fbe80df9
ilian.kostadinov@gmail.com <ilian.kostadinov@gmail.com>
2020-08-26 12:16:31 +0300

    First initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Alien Invasion game"""
+
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def _update_screen(self):
+        """Update the images on the screen, and flip to the new window"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+    def _check_events(self):
+        """Respond to keypress and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            self._check_events()
+            self._update_screen()
+
+            # Make the most recent drawn screen visible.
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
